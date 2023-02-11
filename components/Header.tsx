d4bddc291e1190f82544eb638c2a29e77e6cce3a
SeungGukYoo <dbtmd878@gmail.com>
2023-02-11 17:47:59 +0900

    header.tsx 스크롤시 배경색 변경 이벤트 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { FaBell, FaSearch } from "react-icons/fa";
 import Link from "next/link";
 function Header() {
+  const [scrolled, setScrolled] = useState(false);
+  const handleScroll = useCallback(() => {
+    window.scrollY > 0 ? setScrolled(true) : setScrolled(false);
+  }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header>
+    <header
+      className={`${
+        scrolled && "bg-[#141414]"
+      } transition-colors duration-[.5s]`}
+    >
       <div className="flex item-center space-x-2 md:space-x-10">
         <img
           className="cursor-pointer"
@@ -12,7 +25,7 @@ function Header() {
           width={100}
           height={100}
         />
-        <ul className="space-x-4 md:flex">
+        <ul className="hidden space-x-4 md:flex">
           <li className="headerLink">Home</li>
           <li className="headerLink">TV Shows</li>
           <li className="headerLink">Movies</li>
