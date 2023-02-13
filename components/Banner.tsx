aad38fc89450680252313b6e5a6459e657178e8a
SeungGukYoo <dbtmd878@gmail.com>
2023-02-13 15:33:37 +0900

    배너에 실제 이미지 출력

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,19 +1,30 @@
 import { Movie } from "@/typings";
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import { baseUrl } from "@/constants/movie";
 
 interface Props {
   original: Movie[];
 }
 function Banner({ original }: Props) {
   const [movie, setMovie] = useState<Movie | null>(null);
-  console.log(movie);
+
   useEffect(() => {
     setMovie(original[Math.floor(Math.random() * original.length)]);
   }, [original]);
-
+  console.log(movie);
   return (
     <section>
-      <div>banner</div>
+      <div>
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          alt={`${movie?.title}`}
+          fill
+          priority
+          quality={50}
+          className="object-cover"
+        />
+      </div>
     </section>
   );
 }
