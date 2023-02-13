306e11b04f3106d931dbc81991b286e6c97e031d
SeungGukYoo <dbtmd878@gmail.com>
2023-02-13 13:53:24 +0900

    getServerSideProps 추가 및 필요한 데이터 호출, props 타입 지정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "@/components/Header";
+import requests from "@/utils/requests";
+import { Movie } from "@/typings";
 
-const Home: NextPage = () => {
+interface Props {
+  topRated: Movie[];
+  sf: Movie[];
+  action: Movie[];
+  comedy: Movie[];
+  drama: Movie[];
+  fantasy: Movie[];
+}
+const Home = ({ topRated, sf, action, comedy, drama, fantasy }: Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-[#333] to-[#141414]">
       <Head>
@@ -16,6 +26,29 @@ const Home: NextPage = () => {
       </main>
     </div>
   );
+};
+
+export const getServerSideProps = async () => {
+  const [top, sf, action, comedy, drama, fantasy] = await Promise.all([
+    fetch(requests.top).then((res) => res.json()),
+    fetch(requests.sf).then((res) => res.json()),
+    fetch(requests.action).then((res) => res.json()),
+    fetch(requests.comedy).then((res) => res.json()),
+    fetch(requests.drama).then((res) => res.json()),
+    fetch(requests.fantasy).then((res) => res.json()),
+  ]);
+
+  console.log(top);
+  return {
+    props: {
+      topRated: top.results,
+      sf: sf.results,
+      action: action.results,
+      comedy: comedy.results,
+      drama: drama.results,
+      fantasy: fantasy.results,
+    },
+  };
 };
 
 export default Home;
