9001ddf8b57ab3ce63cc807c806461a7a6d7d2d4
SeungGukYoo <dbtmd878@gmail.com>
2023-02-14 00:07:40 +0900

    리스트에 보여지는 영화 썸네일 컴포넌트 추가 및 스타일링

---
+++ components/thumbnail.tsx
@@ -0,0 +1,25 @@
+import { Movie } from "@/typings";
+import Image from "next/image";
+import { baseUrl } from "@/constants/movie";
+
+interface Props {
+  movie: Movie;
+}
+function Thumbnail({ movie }: Props) {
+  return (
+    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:min-w-[260px] md:hover:scale-105">
+      <Image
+        src={`${baseUrl}/w500${movie.backdrop_path || movie.poster_path}`}
+        alt={`${movie.title}`}
+        fill
+        priority
+        sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+        className="rounded-sm object-cover  md:rounded"
+      />
+    </div>
+  );
+}
+
+export default Thumbnail;
