e16ec4d45e7cb6fbebea9a7b16ed20f9ac86a82b
SeungGukYoo <dbtmd878@gmail.com>
2023-02-14 15:07:28 +0900

    tailwind-scrollbar,scrollbar-hide 플러그인 추가, 커스텀 배경 CSS추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        gradient1:
+          "linear-gradient(to bottom, rgba(20,20,20,0), rgba(20,20,20,1))",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("tailwind-scrollbar"), require("tailwind-scrollbar-hide")],
+  variants: {
+    scrollbar: ["rounded"],
+  },
+};
