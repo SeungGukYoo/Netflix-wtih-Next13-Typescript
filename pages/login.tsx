7e17f3187933df5f06dc1cbd19179dda6cb525ef
SeungGukYoo <dbtmd878@gmail.com>
2023-02-14 16:20:16 +0900

    react-hook-form를 이용한 로그인 유효성 기능 추가

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,29 @@
 import Head from "next/head";
 import Image from "next/image";
-import { baseUrl } from "@/constants/movie";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
+interface Inputs {
+  email: string;
+  password: string;
+}
 function Login() {
+  const [login, setLogin] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const loginHandler: SubmitHandler<Inputs> = ({ email, password }) => {
+    if (login) {
+      console.log(email, password);
+    } else {
+      console.log(email, password);
+    }
+  };
+
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
@@ -26,18 +47,39 @@ function Login() {
         height={150}
         className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
       />
-      <form className="relative mt-24 space-y-8 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+      <form
+        onSubmit={handleSubmit(loginHandler)}
+        className="relative mt-24 space-y-8 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+      >
         <h1 className="text-4xl font-semibold ">Sign In</h1>
         <div className="space-y-4">
-          <input type="email" placeholder="email" className="input" />
-          <input type="password" placeholder="password" className="input" />
+          <input
+            type="email"
+            placeholder="email"
+            className="input"
+            {...register("email", { required: true })}
+          />
+          {errors.email && <span>아이디를 다시 한번 확인해주세요.</span>}
+          <input
+            type="password"
+            placeholder="password"
+            className="input"
+            {...register("password", { required: true })}
+          />
+          {errors.password && <span>비밀번호를 다시 한번 확인해주세요.</span>}
         </div>
-        <button className="w-full rounded bg-[#e40914] py-3 font-semibold">
+        <button
+          onClick={() => setLogin(true)}
+          className="w-full rounded bg-[#e40914] py-3 font-semibold"
+        >
           Sign In
         </button>
         <div className="text-[gray]">
           New to Netflix
-          <button className="text-[white] ml-4 hover:underline">
+          <button
+            onClick={() => setLogin(false)}
+            className="text-[white] ml-4 hover:underline"
+          >
             Sing up Now
           </button>
         </div>
