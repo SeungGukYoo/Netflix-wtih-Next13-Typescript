ed5aebfde43e9ac469acb026740fd5e8b912e26e
SeungGukYoo <dbtmd878@gmail.com>
2023-02-16 00:05:57 +0900

    Atoms state에 영화 데이터 전달 및 모달창 생성 및 제거 함수 추가

--- components/thumbnail.tsx
+++ components/thumbnail.tsx
@@ -1,13 +1,25 @@
 import { Movie } from "@/typings";
 import Image from "next/image";
 import { baseUrl } from "@/constants/movie";
+import { useRecoilState } from "recoil";
+import { modalState, movieState } from "@/atoms/globalAtom";
+import { useCallback } from "react";
 
 interface Props {
   movie: Movie;
 }
 function Thumbnail({ movie }: Props) {
+  const [show, setShow] = useRecoilState(modalState);
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
+  const modalHandler = useCallback(() => {
+    setShow(true);
+    setCurrentMovie(movie);
+  }, []);
   return (
-    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:min-w-[260px] md:hover:scale-105">
+    <div
+      onClick={modalHandler}
+      className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:min-w-[260px] md:hover:scale-105"
+    >
       <Image
         src={`${baseUrl}/w500${movie.backdrop_path || movie.poster_path}`}
         alt={`${movie.title}`}
