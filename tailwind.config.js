e51cc572107d24e3180e305cfbe508e9b93187fd
SeungGukYoo <dbtmd878@gmail.com>
2023-02-15 17:04:43 +0900

    기본 스핀 애니메이션 오류로 인하여 스핀 애니메이션 구현

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         gradient1:
           "linear-gradient(to bottom, rgba(20,20,20,0), rgba(20,20,20,1))",
       },
+      keyframes: {
+        rotation: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
+      animation: {
+        "ani-rotation": "rotation 1.5s linear infinite",
+      },
     },
   },
   plugins: [require("tailwind-scrollbar"), require("tailwind-scrollbar-hide")],
